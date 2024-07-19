26a868afdb09cf6834faa080d21ec0b890328194
DemonKingSwarn <rockingswarn@gmail.com>
2024-07-20 01:33:30 +0530

    chore: Day and Night cycle done

---
+++ Assets/Scripts/DayNightCycle/LightingPreset.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient ambientColor;
+    public Gradient directionalColor;
+    public Gradient fogColor;
+}
