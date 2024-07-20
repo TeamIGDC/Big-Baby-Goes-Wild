3ec728e75e5ab061c2583c40def9bd58e7f809bd
RaghavGohil <71706645+RaghavGohil@users.noreply.github.com>
2024-07-20 15:36:23 +0530

    chore: added player movement and input system + some assets

---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Game.Entity 
+{
+    public abstract class Entity : MonoBehaviour
+    {
+        [SerializeField] protected float _speed;
+
+        protected void Start()
+        {
+        
+        }
+    }
+}
+
